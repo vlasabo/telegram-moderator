8420fe608f37c3d20345f722a4a1138ae4b953d8
Vladimir <vladimirsabo@yandex.ru>
2024-04-10 11:54:38 +0300

    добавить базовые зависимости и compose файл с mongo

---
+++ src/main/java/com/telegram/moderator/ModeratorApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.moderator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegram/moderator/ModeratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegram.moderator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
